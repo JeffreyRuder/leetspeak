95078eb3e445f8d8a762a2e6a2e4c87154262c85
chris houghton and jeff ruder <ruderjt@gmail.com>
2016-01-04 14:44:55 -0800

    Add tests and initial code to pass first test

---
+++ src/main/java/Leetspeak.java
@@ -0,0 +1,14 @@
+public class Leetspeak {
+  public static void main(String[] args) {
+
+  }
+
+  public String leetspeak (String inputString) {
+    return inputString;
+
+
+  }
+
+
+  
+}
--- src/test/java/LeetspeakTest.java
+++ src/test/java/LeetspeakTest.java
@@ -5,44 +5,44 @@ public class LeetspeakTest {
 
   @Test
   public void leetspeak_returnsAStringAsIsWhenNoLeetspeakIsPresent_wordDoesntChange() {
-    AppName yourApp = new AppName();
-    assertEquals("happy", yourApp.leetspeak("happy"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("happy", Leetspeak.leetspeak("happy"));
   }
 
   @Test
   public void leetspeak_replacesEveryEInAStringWithA3_wordChanges() {
-    AppName yourApp = new AppName();
-    assertEquals("3l3phant", yourApp.leetspeak("elephant"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("3l3phant", Leetspeak.leetspeak("elephant"));
   }
 
   @Test
   public void leetspeak_replacesEveryOInAStringWithA0_wordChanges() {
-    AppName yourApp = new AppName();
-    assertEquals("b00 b00", yourApp.leetspeak("boo boo"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("b00 b00", Leetspeak.leetspeak("boo boo"));
   }
 
   @Test
   public void leetspeak_replacesEveryIInAStringWithA1_wordChanges() {
-    AppName yourApp = new AppName();
-    assertEquals("1 lik3 1k3", yourApp.leetspeak("I like Ike"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("1 lik3 1k3", Leetspeak.leetspeak("I like Ike"));
   }
 
   @Test
   public void leetspeak_replacesEverySInAStringWithAZ_wordChanges() {
-    AppName yourApp = new AppName();
-    assertEquals("roz3z are r3d", yourApp.leetspeak("roses are red"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("roz3z are r3d", Leetspeak.leetspeak("roses are red"));
   }
 
   @Test
   public void leetspeak_doesNotReplaceSWhenFirstLetter_wordChanges() {
-    AppName yourApp = new AppName();
-    assertEquals("Suz13 Sunzh1n3", yourApp.leetspeak("Susie Sunshine"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals("Suz13 Sunzh1n3", Leetspeak.leetspeak("Susie Sunshine"));
   }
 
-  @test
+  @Test
   public void leetspeak_makesCorrectReplacementsRegardlessOfCase_ignoresUppercase() {
-    AppName yourApp = new AppName();
-    assertEquals( "1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am!", yourApp.leetspeak("I scream you scream we all scream for raspberry ice cream!"));
+    Leetspeak Leetspeak = new Leetspeak();
+    assertEquals( "1 scr3am y0u scr3am w3 all scr3am f0r razpb3rry ic3 cr3am!", Leetspeak.leetspeak("I scream you scream we all scream for raspberry ice cream!"));
   }
 
 }
